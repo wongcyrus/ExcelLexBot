7b2f27da210daa3962acbc760395c1f34d49692d
EC2 Default User <ec2-user@ip-172-31-89-51.ec2.internal>
2019-10-17 07:02:55 +0000

    Add output parameter for S3 bucket.

--- deployment.sh
+++ deployment.sh
@@ -4,16 +4,15 @@ export REGION=$AWS_DEFAULT_REGION
 
 echo "Deploy $STACK_NAME stack at $REGION region"
 
-sourcebucket=cywong$STACK_NAME
+sourcebucket=excellexbotsource$STACK_NAME
 aws s3 mb s3://$sourcebucket --region $REGION 
 rm excellexbotpackage.yaml
 sam package --template-file excellexbot.yaml --s3-bucket $sourcebucket --output-template-file excellexbotpackage.yaml
 
 aws cloudformation deploy --stack-name $STACK_NAME --template-file excellexbotpackage.yaml \
 --region $REGION --capabilities CAPABILITY_IAM \
---parameter-overrides ExcelBucketName=excellexbotv2 \
-                        DynamodbAutoScaling=false
-                        
+--parameter-overrides DynamodbAutoScaling=false
+
 rm codehookpackage.yaml                     
 sam package --template-file codehook.yaml --s3-bucket $sourcebucket --output-template-file codehookpackage.yaml
 aws cloudformation deploy --stack-name Codehook$STACK_NAME --template-file codehookpackage.yaml \
