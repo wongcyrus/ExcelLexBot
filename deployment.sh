2c4847c54f0e17b0802963c20210c288a9602122
EC2 Default User <ec2-user@ip-172-31-81-169.ec2.internal>
2019-10-21 04:33:35 +0000

    uncomment the deployment command.

--- deployment.sh
+++ deployment.sh
@@ -10,9 +10,9 @@ aws s3 mb s3://$sourcebucket --region $REGION
 rm excellexbotpackage.yaml
 sam package --template-file excellexbot.yaml --s3-bucket $sourcebucket --output-template-file excellexbotpackage.yaml
 
-# aws cloudformation deploy --stack-name $STACK_NAME --template-file excellexbotpackage.yaml \
-# --region $REGION --capabilities CAPABILITY_IAM \
-# --parameter-overrides DynamodbAutoScaling=false
+aws cloudformation deploy --stack-name $STACK_NAME --template-file excellexbotpackage.yaml \
+--region $REGION --capabilities CAPABILITY_IAM \
+--parameter-overrides DynamodbAutoScaling=false
 
 # uncomment the following if you want to deploy the codehook example project.
 # rm codehookpackage.yaml                     
