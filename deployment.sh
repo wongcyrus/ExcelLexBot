690d7887a08c1ca2ee805fd6924ddef00920c0f7
EC2 Default User <ec2-user@ip-172-31-82-250.ec2.internal>
2021-06-17 07:54:24 +0000

    Update packages.

--- deployment.sh
+++ deployment.sh
@@ -10,11 +10,11 @@ aws s3 mb s3://$sourcebucket --region $REGION
 rm excellexbotpackage.yaml
 sam package --template-file excellexbot.yaml --s3-bucket $sourcebucket --output-template-file excellexbotpackage.yaml
 
-aws cloudformation deploy --stack-name $STACK_NAME --template-file excellexbotpackage.yaml \
---region $REGION --capabilities CAPABILITY_IAM \
---parameter-overrides DynamodbAutoScaling=false
+# aws cloudformation deploy --stack-name $STACK_NAME --template-file excellexbotpackage.yaml \
+# --region $REGION --capabilities CAPABILITY_IAM \
+# --parameter-overrides DynamodbAutoScaling=false
 
-# sam publish --template excellexbotpackage.yaml --region us-east-1
+sam publish --template excellexbotpackage.yaml --region us-east-1
 
 # uncomment the following if you want to deploy the codehook example project.
 # rm codehookpackage.yaml                     
--- setup.sh
+++ setup.sh
@@ -1,20 +1,12 @@
-# Change to default Python3
-alias python='python3'
-python --version
 # Install Code Formatter for Python and you need to set AWS Cloud9「Preferences」->「Python Support」->「Custom Code Formatter」
 # yapf -i "$file"
 sudo pip install yapf
 sudo yum -y update
 sudo yum -y install aws-cli
 sudo -H pip install awscli --upgrade
-# Install brew and update SAM CLI to the latest version.
-sudo ln -s /bin/touch /usr/bin/touch
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-brew --version
-brew tap aws/tap
-brew install aws-sam-cli
+# Install update SAM CLI to the latest version.
+wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install --update
+rm -rf sam-installation
 sam --version
